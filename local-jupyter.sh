9fdd4db9a476ebf5249a165c011178a8b06d0a23
Diego Herrera <dherrera@redhat.com>
2025-09-04 15:38:12 -0400

    jupyverse is still too green, maybe in a couple of years

--- local-jupyter.sh
+++ local-jupyter.sh
@@ -15,11 +15,12 @@ podman run -d --rm \
     -v $(pwd)/jupyter_stuff:/jupyter_stuff:Z \
     --workdir=/jupyter_stuff \
     localhost/jupyter-local-image:latest \
-    /opt/venv/bin/jupyverse \
-        --host 0.0.0.0 \
-        --port 8888 \
-        --set frontend.collaborative=true \
-        --set auth.mode=noauth
+    python3 -m jupyterlab \
+        --ServerApp.ip=0.0.0.0 \
+        --ServerApp.port=8888 \
+        --LabApp.collaborative=True \
+        --IdentityProvider.token= \
+        --no-browser
 podman logs -f --color jupyter-local &
 
 # Run firefox inside container network
--- shared-jupyter.sh
+++ shared-jupyter.sh
@@ -15,11 +15,11 @@ podman unshare chown $UID:$UID -R $(pwd)/jupyter_stuff
     --workdir=/jupyter_stuff \
     -p=8888:8888 \
     localhost/jupyter-local-image:latest \
-    /opt/venv/bin/jupyverse \
-        --host 0.0.0.0 \
-        --port 8888 \
-        --set frontend.collaborative=true \
-        --set auth.mode=token \
+    /opt/venv/bin/python -m jupyterlab \
+        --ServerApp.ip=0.0.0.0 \
+        --ServerApp.port=8888 \
+        --LabApp.collaborative=True \
+        --no-browser
 ) && true
 
 ERR=$?
